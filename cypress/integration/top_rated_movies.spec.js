20aac4a34894f2f1d4e6fa370e6201866e413f05
Jyotsna Srivastava <jyotsna.netherlands@gmail.com>
2022-03-02 01:21:11 +0100

    top rated test file

--- cypress/integration/top_rated_movies.spec.js
+++ cypress/integration/top_rated_movies.spec.js
@@ -1,6 +1,28 @@
-// top_rated_movies.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+describe('Top Rated Movies API', () => {
+    context('GET /Top Rated Movie List', () => {
+        const base_url='https://developers.themoviedb.org/3/movies/rate-movie'
+        it('send a valid api key and get 200 response code', () => {
+            cy.request({
+                method: 'GET',
+                end_point: '/movie/top_rated',
+                api_key: 'e3c9e03e7e810aba89b7828365892a92'
+            })
+            .should((response) => {
+                    expect(response.status).to.eq(200)
+                    cy.log(JSON.stringify(response.body))
+                });
+        });
+        
+        it('send a invalid authentication credentials & get 401 Unauthorized response code', () => {
+            cy.request({
+                method: 'GET',
+                end_point: '/movie/top_rated',
+                api_key: 'e3c9e03e7e810aba89b7828365892a92'
+            })
+                .should((response) => {
+                    expect(response.status).to.eq(400)
+                    cy.log(JSON.stringify(response.body))
+                });
+        });
+    });
+});
